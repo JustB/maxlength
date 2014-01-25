db8a142830cef862c3fe804923a20e9bab7adc13
Giustino Borzacchiello <giustinob@gmail.com>
2014-01-25 13:36:44 +0100

    created jquery collection method

--- jquery.maxlength.js
+++ jquery.maxlength.js
@@ -7,12 +7,22 @@
 			feedbackText: '{r} characters remaining ({m} maximum)'
 		};
 		this._defaults = {
-			max: 200 // Maxmim length
+			max: 200 // Maximum length
 		};
 		$.extend(this._defaults, this.regional['']);
 	}
 
 	$.extend(MaxLength.prototype, {});
+
+	/* Attach the max length functionality to a jQuery selection.
+		@param options (objec) the new settings to use for these instances (optional)
+		@return (jQuery) for chaining further calls */
+	$.fn.maxlength = function (options) {
+		// this refers to a jQuery collection, it should not be wrapper in jQuery
+		return this.each(function (){
+			plugin._attachPlugin(this, options || {});
+		});
+	};
 
 	/* Initialize the max length functionality */
 	var plugin = $.maxlength = new MaxLength();
