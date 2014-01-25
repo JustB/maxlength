01b2c818fdef5b2fac1d3192e20a18190b02892d
Giustino Borzacchiello <giustinob@gmail.com>
2014-01-25 13:55:17 +0100

    initialize plugin

--- jquery.maxlength.js
+++ jquery.maxlength.js
@@ -12,7 +12,29 @@
 		$.extend(this._defaults, this.regional['']);
 	}
 
-	$.extend(MaxLength.prototype, {});
+	$.extend(MaxLength.prototype, {
+		_attachPlugin: function(target, options) {
+			target = $(target);
+			if ( target.hasClass(this.markerClassName) ) {
+				return;
+			}
+
+			var inst = {options: $.extend({}, this._defaults), feedbackTarget: $([])};
+			target.addClass(this.markerClassName).
+				data(this.propertyName, inst).
+				bind('keypress.maxlength', function(event) {
+					if (!inst.options.truncate) {
+						return true;
+					}
+					var ch = String.fromCharCode(event.charCode == undefined ? event.keyCode : event.charCode);
+					return (event.ctrlKey || event.metaKey || ch == '\u0000' || $(this).val().length < inst.options.max);
+				}).
+				bind('keyup.maxlength', function() {
+					plugin._checkLength($(this));
+				});
+			this._optionPlugin(target, options);
+		}
+	});
 
 	/* Attach the max length functionality to a jQuery selection.
 		@param options (objec) the new settings to use for these instances (optional)
